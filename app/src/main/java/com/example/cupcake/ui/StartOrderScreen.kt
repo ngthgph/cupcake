a86fc811a2f2cc41b3245cfa7b8c1f56d9f57df9
ngthgph <ngthgph112@gmail.com>
2023-11-09 19:11:44 +0700

    Add button handlers

--- app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
@@ -49,6 +49,7 @@ import com.example.cupcake.data.DataSource
 @Composable
 fun StartOrderScreen(
     quantityOptions: List<Pair<Int, Int>>,
+    onQuantitySelected: (Int) -> Unit,
     modifier: Modifier = Modifier
 ){
     Column(
@@ -84,7 +85,7 @@ fun StartOrderScreen(
                 quantityOptions.forEach { item ->
                     SelectQuantityButton(
                         labelResourceId = item.first,
-                        onClick = {}
+                        onClick = { onQuantitySelected(item.second) }
                     )
                 }
             }
@@ -115,6 +116,7 @@ fun SelectQuantityButton(
 fun StartOrderPreview(){
     StartOrderScreen(
         quantityOptions = DataSource.quantityOptions,
+        onQuantitySelected = {},
         modifier = Modifier.fillMaxSize().padding(dimensionResource(R.dimen.padding_medium))
     )
 }
