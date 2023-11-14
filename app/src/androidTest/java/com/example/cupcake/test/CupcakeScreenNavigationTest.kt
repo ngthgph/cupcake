8cd7ad5f658451b44fc411e315fc689d42d4601c
ngthgph <ngthgph112@gmail.com>
2023-11-14 12:52:04 +0700

    Verify Back Nav not on Start Destination

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -3,10 +3,12 @@ package com.example.cupcake.test
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.example.cupcake.CupcakeApp
 import com.example.cupcake.CupcakeScreen
+import com.example.cupcake.R
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -29,5 +31,10 @@ class CupcakeScreenNavigationTest {
     @Test
     fun cupcakeNavHost_verifyStartDestination() {
         navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+    @Test
+    fun cupcakeNavHost_verifyBackNavigationNotShownOnStartDestination() {
+        val backText = composeTestRule.activity.getString(R.string.back_button)
+        composeTestRule.onNodeWithContentDescription(backText).assertDoesNotExist()
     }
 }
